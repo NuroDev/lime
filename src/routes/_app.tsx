06774c12a3996d87d7b70fae7f7dd033fcef14fa
nurodev <me@nuro.dev>
2023-08-22 00:18:21 +0100

    Added description to `_app.tsx`

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -5,6 +5,10 @@ export default defineApp((_req, { Component }) => (
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="An opinionated Deno Fresh project template"
+      />
       <title>Lime</title>
     </head>
     <body class="antialiased bg-gray-50 text-gray-600">
