35283ad61c63413f0f53be6d5f773c4e3549dce8
nurodev <me@nuro.dev>
2023-08-31 20:24:35 +0100

    Updated Fresh routes gen

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -4,14 +4,16 @@
 
 import * as $0 from './routes/_404.tsx';
 import * as $1 from './routes/_app.tsx';
-import * as $2 from './routes/index.tsx';
+import * as $2 from './routes/_layout.tsx';
+import * as $3 from './routes/index.tsx';
 import * as $$0 from './islands/Counter.tsx';
 
 const manifest = {
     routes: {
         './routes/_404.tsx': $0,
         './routes/_app.tsx': $1,
-        './routes/index.tsx': $2,
+        './routes/_layout.tsx': $2,
+        './routes/index.tsx': $3,
     },
     islands: {
         './islands/Counter.tsx': $$0,
