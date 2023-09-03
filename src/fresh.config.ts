0d1be3ee7b31fd907330b822c90a865700f367f7
nurodev <me@nuro.dev>
2023-09-03 17:28:47 +0100

    Added `fresh_seo` plugin to Fresh config

--- src/fresh.config.ts
+++ src/fresh.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from '$fresh/server.ts';
 import twindPlugin from '$fresh/plugins/twind.ts';
+import { freshSEOPlugin } from 'fresh_seo';
+
+import manifest from '~/fresh.gen.ts';
 import twindConfig from './twind.config.ts';
 
 export default defineConfig({
-    plugins: [twindPlugin(twindConfig)],
+    plugins: [
+        freshSEOPlugin(manifest, {
+            exclude: [],
+            include: [],
+        }),
+        twindPlugin(twindConfig),
+    ],
     port: Number(Deno.env.get('PORT')) || 8000,
 });
