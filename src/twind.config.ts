d7e54babdb249ea020727499ed4654999701e417
nurodev <me@nuro.dev>
2023-09-12 00:41:44 +0100

    Updated default twind config

--- src/twind.config.ts
+++ src/twind.config.ts
@@ -1,6 +1,26 @@
-import { Options } from '$fresh/plugins/twind.ts';
+import * as defaultColors from 'twind/colors';
+import type { Options } from '$fresh/plugins/twind.ts';
 
 export default {
     darkMode: 'class',
     selfURL: import.meta.url,
-} as Options;
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    50: '#f9fafb',
+                    100: '#eaeaeb',
+                    200: '#cacbcd',
+                    300: '#a7a9ac',
+                    400: '#696c71',
+                    500: '#282d34',
+                    600: '#24292f',
+                    700: '#181b20',
+                    800: '#121518',
+                    900: '#0c0e10',
+                },
+                primary: defaultColors.rose,
+            },
+        },
+    },
+} satisfies Options;
