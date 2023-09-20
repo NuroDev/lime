afdab483488283e29b71c156adf993a6ab5322b2
nurodev <me@nuro.dev>
2023-09-20 23:26:06 +0100

    Added Fresh context to tRPC

--- src/routes/api/trpc/[...path].ts
+++ src/routes/api/trpc/[...path].ts
@@ -4,9 +4,9 @@ import { appRouter, createContext } from '~/trpc/mod.ts';
 
 import type { Handler } from '$fresh/server.ts';
 
-export const handler: Handler = (req) =>
+export const handler: Handler = (req, ctx) =>
     fetchRequestHandler({
-        createContext,
+        createContext: createContext(ctx),
         endpoint: '/api/trpc',
         req,
         router: appRouter,
--- src/trpc/context.ts
+++ src/trpc/context.ts
@@ -2,11 +2,22 @@ import superjson from 'superjson';
 import { initTRPC } from '@trpc/server';
 
 import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
+import type { HandlerContext } from '$fresh/server.ts';
 
-export const t = initTRPC.create({
+interface Context {
+    freshContext: HandlerContext;
+    req: Request;
+}
+
+export function createContext(
+    freshContext: HandlerContext,
+): (opts: FetchCreateContextFnOptions) => Context {
+    return ({ req }) => ({
+        freshContext,
+        req,
+    });
+}
+
+export const t = initTRPC.context<Context>().create({
     transformer: superjson,
 });
-
-export function createContext(_options: FetchCreateContextFnOptions) {
-    return {};
-}
