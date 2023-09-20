bce3200f30f47b69ca804fb8b8e8102b92c89a9c
nurodev <me@nuro.dev>
2023-09-20 15:26:00 +0100

    Added KV path directory creation logic

--- src/database/kv.ts
+++ src/database/kv.ts
@@ -1,4 +1,14 @@
+import { dirname, resolve } from '$std/path/mod.ts';
+import { existsSync } from '$std/fs/mod.ts';
+
+import '$std/dotenv/load.ts';
+
 const KV_PATH = Deno.env.get('KV_PATH') || './kv/local.db';
+
+const resolvedKvDir = resolve(dirname(KV_PATH));
+if (!existsSync(resolvedKvDir)) {
+    Deno.mkdirSync(resolvedKvDir, { recursive: true });
+}
 
 export const kv = await Deno.openKv(
     KV_PATH,
