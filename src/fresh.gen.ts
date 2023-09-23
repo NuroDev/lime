275f2b2c836566e475c63321d2fc036b2462a735
nurodev <me@nuro.dev>
2023-09-24 00:33:51 +0100

    Added initial WIP register page

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -3,24 +3,26 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from './routes/(auth)/login.tsx';
-import * as $1 from './routes/_404.tsx';
-import * as $2 from './routes/_app.tsx';
-import * as $3 from './routes/_layout.tsx';
-import * as $4 from './routes/_middleware.ts';
-import * as $5 from './routes/api/trpc/[...path].ts';
-import * as $6 from './routes/index.tsx';
+import * as $1 from './routes/(auth)/register.tsx';
+import * as $2 from './routes/_404.tsx';
+import * as $3 from './routes/_app.tsx';
+import * as $4 from './routes/_layout.tsx';
+import * as $5 from './routes/_middleware.ts';
+import * as $6 from './routes/api/trpc/[...path].ts';
+import * as $7 from './routes/index.tsx';
 import * as $$0 from './islands/Counter.tsx';
 import * as $$1 from './islands/Fetch.tsx';
 
 const manifest = {
     routes: {
         './routes/(auth)/login.tsx': $0,
-        './routes/_404.tsx': $1,
-        './routes/_app.tsx': $2,
-        './routes/_layout.tsx': $3,
-        './routes/_middleware.ts': $4,
-        './routes/api/trpc/[...path].ts': $5,
-        './routes/index.tsx': $6,
+        './routes/(auth)/register.tsx': $1,
+        './routes/_404.tsx': $2,
+        './routes/_app.tsx': $3,
+        './routes/_layout.tsx': $4,
+        './routes/_middleware.ts': $5,
+        './routes/api/trpc/[...path].ts': $6,
+        './routes/index.tsx': $7,
     },
     islands: {
         './islands/Counter.tsx': $$0,
---
+++ src/routes/(auth)/register.tsx
@@ -0,0 +1,68 @@
+import { GithubIcon } from 'lucide-preact';
+import { Head } from '$fresh/runtime.ts';
+
+import { LoginButton } from '~/components/auth/Button.tsx';
+import { Logo } from '~/components/Logo.tsx';
+
+export default function RegisterPage() {
+    return (
+        <>
+            <Head>
+                <title>Register ─ Lime</title>
+            </Head>
+
+            <div class='flex items-center justify-center min-h-screen'>
+                <div class='sm:mx-0 w-full max-w-sm mx-4 space-y-8'>
+                    <div class='space-y-4'>
+                        <div class='flex justify-center items-center'>
+                            <a
+                                href='/'
+                                class='flex justify-center hover:bg-gray-100 dark:hover:bg-gray-800 p-1 rounded-lg transition ease-in-out duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 dark:focus:ring-offset-gray-900'
+                            >
+                                <Logo class='w-auto h-16 mx-auto' />
+                            </a>
+                        </div>
+
+                        <h2 class='text-3xl font-extrabold text-center text-gray-900 dark:text-white'>
+                            Sign up to your account
+                        </h2>
+
+                        <p class='mt-2 text-sm text-center text-gray-400'>
+                            Already have an account?{' '}
+                            <a
+                                class='text-primary-600 hover:text-primary-700 default-transition default-focus font-bold rounded-lg'
+                                href='/login'
+                            >
+                                Login to your account
+                            </a>
+                        </p>
+                    </div>
+
+                    <div class='space-y-4'>
+                        <LoginButton icon={GithubIcon}>
+                            Sign up with{' '}
+                            <span className='capitalize'>GitHub</span>
+                        </LoginButton>
+                    </div>
+
+                    <p class='mt-2 text-sm text-center text-gray-400'>
+                        By signing up, you agree to our <br />{' '}
+                        <a
+                            class='text-primary-600 hover:text-primary-700 default-transition default-focus font-bold rounded-lg'
+                            href='/terms'
+                        >
+                            Terms of Service
+                        </a>{' '}
+                        &{' '}
+                        <a
+                            class='text-primary-600 hover:text-primary-700 default-transition default-focus font-bold rounded-lg'
+                            href='/privacy'
+                        >
+                            Privacy Policy
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
