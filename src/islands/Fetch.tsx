81cedad879f730000227d840c7c92e01700be596
nurodev <me@nuro.dev>
2023-09-24 00:39:55 +0100

    Updated `Fetch` island component

--- src/islands/Fetch.tsx
+++ src/islands/Fetch.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'preact/hooks';
+import { useCallback, useEffect } from 'preact/hooks';
 import { useSignal } from '@preact/signals';
 
 import { client } from '~/trpc/client.ts';
@@ -6,19 +6,19 @@ import { client } from '~/trpc/client.ts';
 export function Fetch() {
     const message = useSignal<string | null>(null);
 
-    useEffect(() => {
-        async function fetchHello(): Promise<void> {
-            try {
-                const response = await client.hello.query({
-                    name: 'from tRPC!',
-                });
+    const fetchHello = useCallback(async () => {
+        try {
+            const response = await client.hello.query({
+                name: 'from tRPC!',
+            });
 
-                message.value = response.message;
-            } catch (error) {
-                console.error(error);
-            }
+            message.value = response.message;
+        } catch (error) {
+            console.error(error);
         }
+    }, []);
 
+    useEffect(() => {
         fetchHello();
     }, []);
 
