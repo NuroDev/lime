61abd312f03c79c545fa46829ab2220cba2ffc0e
nurodev <me@nuro.dev>
2023-09-24 00:33:44 +0100

    Moved login page `LoginButton` to shared component

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -1,61 +1,8 @@
 import { GithubIcon } from 'lucide-preact';
 import { Head } from '$fresh/runtime.ts';
 
-import { cn } from '~/utils/cn.ts';
+import { LoginButton } from '~/components/auth/Button.tsx';
 import { Logo } from '~/components/Logo.tsx';
-
-import type { JSX } from 'preact';
-import type { LucideIcon } from 'lucide-preact';
-
-interface LoginButtonProps
-    extends Omit<JSX.HTMLAttributes<HTMLButtonElement>, 'icon'> {
-    icon?: LucideIcon;
-    provider: string;
-}
-
-function LoginButton(
-    {
-        children,
-        icon: Icon,
-        provider,
-        ...rest
-    }: LoginButtonProps,
-) {
-    return (
-        <button
-            class={cn(
-                'group relative flex items-center justify-center w-full p-3 h-13',
-                'hover:bg-gray-100/50 dark:hover:bg-gray-800',
-                'font-medium text-gray-400 dark:text-white',
-                'border-2 border-gray-100 hover:border-gray-200/25 dark:border-gray-800 dark:hover:border-gray-700',
-                'rounded-lg',
-                'transition ease-in-out duration-300',
-                'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 dark:focus:ring-offset-gray-900',
-            )}
-            // onClick={() => signIn(provider.id, { callbackUrl: '/teams' })}
-            type='button'
-            {...rest}
-        >
-            {Icon
-                ? (
-                    <span class='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-                        <Icon
-                            aria-hidden={true}
-                            // aria-label={`${provider.name} logo`}
-                            class='w-5 h-5'
-                        />
-                    </span>
-                )
-                : null}
-
-            <span>
-                Sign in with <span className='capitalize'>{provider}</span>
-            </span>
-
-            {children}
-        </button>
-    );
-}
 
 export default function LoginPage() {
     return (
@@ -92,7 +39,10 @@ export default function LoginPage() {
                     </div>
 
                     <div class='space-y-4'>
-                        <LoginButton provider='GitHub' icon={GithubIcon} />
+                        <LoginButton icon={GithubIcon}>
+                            Sign in with{' '}
+                            <span className='capitalize'>GitHub</span>
+                        </LoginButton>
                     </div>
 
                     <p class='mt-2 text-sm text-center text-gray-400'>
