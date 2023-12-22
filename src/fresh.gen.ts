6bb691cbef73a082c94c88be3262c9fe888483a9
nurodev <ben@nuro.dev>
2023-12-22 18:30:10 +0000

    Updated manifest

--- src/fresh.gen.ts
+++ src/fresh.gen.ts
@@ -2,39 +2,44 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from './routes/(auth)/login.tsx';
-import * as $1 from './routes/(auth)/register.tsx';
-import * as $2 from './routes/(marketing)/privacy.ts';
-import * as $3 from './routes/(marketing)/terms.ts';
-import * as $4 from './routes/_404.tsx';
-import * as $5 from './routes/_app.tsx';
-import * as $6 from './routes/_layout.tsx';
-import * as $7 from './routes/_middleware.ts';
-import * as $8 from './routes/api/og.tsx';
-import * as $9 from './routes/api/trpc/[...path].ts';
-import * as $10 from './routes/index.tsx';
-import * as $$0 from './islands/Counter.tsx';
-import * as $$1 from './islands/Fetch.tsx';
+import * as $_auth_login from './routes/(auth)/login.tsx';
+import * as $_auth_register from './routes/(auth)/register.tsx';
+import * as $_marketing_privacy from './routes/(marketing)/privacy.ts';
+import * as $_marketing_terms from './routes/(marketing)/terms.ts';
+import * as $_404 from './routes/_404.tsx';
+import * as $_app from './routes/_app.tsx';
+import * as $_layout from './routes/_layout.tsx';
+import * as $_middleware from './routes/_middleware.ts';
+import * as $api_og from './routes/api/og.tsx';
+import * as $api_trpc_path_ from './routes/api/trpc/[...path].ts';
+import * as $api_v1_path_ from './routes/api/v1/[...path].ts';
+import * as $api_v1_trpc_path_ from './routes/api/v1/trpc/[...path].ts';
+import * as $index from './routes/index.tsx';
+import * as $Counter from './islands/Counter.tsx';
+import * as $Fetch from './islands/Fetch.tsx';
+import { type Manifest } from '$fresh/server.ts';
 
 const manifest = {
     routes: {
-        './routes/(auth)/login.tsx': $0,
-        './routes/(auth)/register.tsx': $1,
-        './routes/(marketing)/privacy.ts': $2,
-        './routes/(marketing)/terms.ts': $3,
-        './routes/_404.tsx': $4,
-        './routes/_app.tsx': $5,
-        './routes/_layout.tsx': $6,
-        './routes/_middleware.ts': $7,
-        './routes/api/og.tsx': $8,
-        './routes/api/trpc/[...path].ts': $9,
-        './routes/index.tsx': $10,
+        './routes/(auth)/login.tsx': $_auth_login,
+        './routes/(auth)/register.tsx': $_auth_register,
+        './routes/(marketing)/privacy.ts': $_marketing_privacy,
+        './routes/(marketing)/terms.ts': $_marketing_terms,
+        './routes/_404.tsx': $_404,
+        './routes/_app.tsx': $_app,
+        './routes/_layout.tsx': $_layout,
+        './routes/_middleware.ts': $_middleware,
+        './routes/api/og.tsx': $api_og,
+        './routes/api/trpc/[...path].ts': $api_trpc_path_,
+        './routes/api/v1/[...path].ts': $api_v1_path_,
+        './routes/api/v1/trpc/[...path].ts': $api_v1_trpc_path_,
+        './routes/index.tsx': $index,
     },
     islands: {
-        './islands/Counter.tsx': $$0,
-        './islands/Fetch.tsx': $$1,
+        './islands/Counter.tsx': $Counter,
+        './islands/Fetch.tsx': $Fetch,
     },
     baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
